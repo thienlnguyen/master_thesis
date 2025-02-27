b8dca234084f7374e5edfeb3beaa799aa6bb55cc
thien <s0569834@htw-berlin.de>
2025-02-27 20:06:34 +0100

    adjust config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [
     vue(),
     vueDevTools(),
@@ -24,3 +24,4 @@ export default defineConfig({
     },
   },
 })
+
