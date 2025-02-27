5ad4844a961fe778586ec32cb922f0e6eefdd143
thien <s0569834@htw-berlin.de>
2025-02-27 23:34:28 +0100

    adjust config

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,46 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from 'node:url'
+
 import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
+  base: "/",
   plugins: [
     vue(),
+    vueDevTools(),
     VitePWA({
       registerType: "autoUpdate",
       manifest: {
-        name: "Meine Vue App",
-        short_name: "VueApp",
-        description: "Meine coole Vue PWA",
+        name: "Food Rec",
+        short_name: "FoodRec",
+        description: "Food Recognition",
         theme_color: "#ffffff",
         background_color: "#ffffff",
         display: "standalone",
         icons: [
           {
-            src: "/icon-192x192.png",
+            src: "/uploads/images.jpeg",
             sizes: "192x192",
-            type: "image/png"
-          },
-          {
-            src: "/icon-512x512.png",
-            sizes: "512x512",
             type: "image/png"
           }
         ]
       }
     })
-  ]
-});
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://master-thesis-gyzs.onrender.com',
+        changeOrigin: true,
+      },
+    },
+  },
+})
+
