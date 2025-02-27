15d9b79d0c457988cea7354ea42159e0b886b1b1
thien <s0569834@htw-berlin.de>
2025-02-27 22:55:59 +0100

    Added PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,32 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "/",
   plugins: [
     vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://master-thesis-gyzs.onrender.com',
-        changeOrigin: true,
-      },
-    },
-  },
-})
-
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Meine Vue App",
+        short_name: "VueApp",
+        description: "Meine coole Vue PWA",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ]
+});
